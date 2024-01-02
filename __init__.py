1a12afab7be5c16aa11af5e2680f81eedd73628b
RUACODER <155351133+RUACODER@users.noreply.github.com>
2024-01-02 11:15:55 -0500

    Update __init__.py
    Add ROTATE_AMOUNT as a variable and SIM_SLOW_SPEED

--- __init__.py
+++ __init__.py
@@ -6,15 +6,16 @@ CENTER_COLOR_DOT = (000, 255, 000)
 ALIGNMENT_LINE_COLOR = (238, 244, 21)
 VISION_COLOR = (0, 255, 0)
 CENTER_OF_MASS_LINE_COLOR = (0, 0, 255)
+ROTATE_AMOUNT = 1
+SIM_SLOW_SPEED = 1
 
 
 MOVE_SPEED = 5 # amount of pixels a boid will move per frame
-ROTATION = 18
+ROTATION = 6
 
-NUM_BOIDS = 40
+NUM_BOIDS = 60
 BOID_SCALE = 5 # how many times larger should boids be than base size (4 by 2 pixels)
 
-VISION_RADIUS = 300 # how far boids can see
-FEELING_RADIUS = 50
+VISION_RADIUS = 200 # how far boids can see
 
 SLEEP_TIME = 17 # how long to wait inbetween frames in milliseconds
