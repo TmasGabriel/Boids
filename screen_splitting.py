38bfdbd87d5f08c91d406dee4238b77884baf6e6
TmasGabriel <thomas303gabriel@gmail.com>
2024-01-14 18:17:40 -0500

    ouchie bears

--- screen_splitting.py
+++ screen_splitting.py
@@ -2,70 +2,59 @@ from boid import *
 from __init__ import *
 
 
-def find_mid(top, bot, left, right):
+def find_mid(left, right, top, bot):
     middle = round((right + left) / 2), round((bot + top) / 2)
 
     return middle
 
 
-def find_dim(top, bot, left, right):
+def find_dimensions(left, right, top, bot):
     width = right - left
     height = bot - top
     return width, height
 
-def detect_quad(boid, top, bot, left, right, mid):
+
+def detect_quad(boid, left, right, top, bot, mid):
     quad = 0
-    t, b, l, r = 0, 0, 0, 0
-    if mid[0] < boid.center[0] < right:
-        if top < boid.center[1] < mid[1]:
+    left_bound, right_bound, top_bound, bot_bound = None, None, None, None
+    if top < boid.center[1] < mid[1]:
+        if mid[0] < boid.center[0] < right:
             quad = 1
-            t, b, l, r = top, mid[1], mid[0], right
-        elif mid[1] < boid.center[1] < bot:
-            quad = 4
-            t, b, l, r = mid[1], bot, mid[0], right
-        else:
-            print('out of bounds')
-    elif left < boid.center[0] < mid[0]:
-        if top < boid.center[1] < mid[1]:
+            left_bound, right_bound, top_bound, bot_bound = mid[0], right, top, mid[1]
+        elif left < boid.center[0] < mid[0]:
             quad = 2
-            t, b, l, r = top, mid[1], left, mid[0]
-        elif mid[1] < boid.center[1] < bot:
-            quad = 3
-            t, b, l, r = mid[1], bot, left, mid[0]
+            left_bound, right_bound, top_bound, bot_bound = mid[0], right, mid[1], bot
         else:
-            print('out of bounds')
+            print('out of bounds 1')
+
+    elif mid[1] < boid.center[1] < bot:
+        if left < boid.center[0] < mid[0]:
+            quad = 3
+            left_bound, right_bound, top_bound, bot_bound = left, mid[0], mid[1], bot
+        elif mid[0] < boid.center[0] < right:
+            quad = 4
+            left_bound, right_bound, top_bound, bot_bound = mid[0], right, mid[1], bot
+        else:
+            print('out of bounds 2')
     else:
-        print('out of bounds')
+        print('out of bounds 3')
 
-    return quad, t, b, l, r
+    return quad, left_bound, right_bound, top_bound, bot_bound
+
+def find_adj(left, right, top, bot, width, height):
+    left_bound = left - width
+    right_bound = right + width
+    top_bound = top - height
+    bot_bound = bot + height
+    return left_bound, right_bound, top_bound, bot_bound
 
 
-def find_adj(quad, top, bot, left, right):
-    width, height = find_dim(top, bot, left, right)
-    t = top - height
-    b = bot + height
-    l = left - width
-    r = right + width
-    return t, b, l, r
-    """
-    if quad == 1:
-        t = top - height
-        b = bot + height
-        l = left - width
-        r = right + width
-        return t, b, l, r
-    elif quad == 2:
-        t = bot + (height * 2)
-        l = left - (width * 2)
-        return t, bot, l, right
-    elif quad == 3:
-        b = bot - (height * 2)
-        l = left - (width * 2)
-        return top, b, l, right
-    elif quad == 4:
-        b = bot - (height * 2)
-        r = left + (width * 2)
-        return top, b, left, r
-    else:
-        print('out of bounds 2')
-    """
+def generate_quad_id(boid, depth):
+    left, right, top, bot = 0, CANVAS_WIDTH, 0, CANVAS_HEIGHT
+    middle = find_mid(left, right, top, bot)
+
+    for i in range(depth):
+        quad, left, right, top, bot = detect_quad(boid, left, right, top, bot, middle)
+        middle = find_mid(left, right, top, bot)
+
+    return left, right, top, bot, middle
